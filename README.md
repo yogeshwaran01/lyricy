ce4df03b9d578f0512e85564e927b22a856f9a07
YOGESHWARAN R <yogeshin247@gmail.com>
2022-07-05 13:46:42 +0530

    updated

---
+++ README.md
@@ -0,0 +1,11 @@
+# lyricy
+
+A simple Python script add lyrics to offline songs
+
+## Usage
+
+```bash
+python lyricy.py <song_path>
+```
+
+It will search and add the lyric to that songs
--- lyricy.py
+++ lyricy.py
@@ -5,18 +5,16 @@ import music_tag
 
 search_link = "https://rclyricsband.com/?s="
 
+
 def search_lyrics(song_name: str, one=True):
     print(f"Searching for {song_name}")
     markup = requests.get(search_link + quote_plus(song_name)).text
-    soup = BeautifulSoup(markup, 'html.parser')
-    outer_tags = soup.find_all('h2', {'class': 'search-entry-title'})
+    soup = BeautifulSoup(markup, "html.parser")
+    outer_tags = soup.find_all("h2", {"class": "search-entry-title"})
     results = []
     for outer_tag in outer_tags:
-        inner_tag = outer_tag.find('a')
-        results.append({
-            'title': inner_tag.get('title'),
-            'link': inner_tag.get('href')
-        })
+        inner_tag = outer_tag.find("a")
+        results.append({"title": inner_tag.get("title"), "link": inner_tag.get("href")})
     if len(results) == 0:
         return None
     if one:
@@ -26,23 +24,21 @@ def search_lyrics(song_name: str, one=True):
         results
 
 
-
 def get_lyrics(link: str):
     print("Getting Lyrics ...")
     markup = requests.get(link).text
-    soup = BeautifulSoup(markup, 'html.parser')
-    return soup.find('div', {'class': 'su-box su-box-style-default'}).text
-
+    soup = BeautifulSoup(markup, "html.parser")
+    return soup.find("div", {"class": "su-box su-box-style-default"}).text
 
 
 def set_lyrics_to_song(song_path: str):
     f = music_tag.load_file(song_path)
-    title = str(f['title'])
+    title = str(f["title"])
     search_results = search_lyrics(title)
     if search_results:
-        lyrics = get_lyrics(search_results['link'])
+        lyrics = get_lyrics(search_results["link"])
         print("Setting Lyrics ...")
-        f['lyrics'] = lyrics
+        f["lyrics"] = lyrics
         f.save()
         print("Done :)")
     else:
@@ -51,5 +47,6 @@ def set_lyrics_to_song(song_path: str):
 
 if __name__ == "__main__":
     import sys
+
     path = sys.argv[1]
     set_lyrics_to_song(path)
